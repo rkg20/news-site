21f61afa7898d077b1d29e30446ac1fb8a19b4d6
RAHUL KUMAR GUPTA <54279173+rkg20@users.noreply.github.com>
2022-01-25 22:36:41 +0530

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2020 BiharKranti.com | <a href="http://BiharKranti.com/">Contact Us</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
